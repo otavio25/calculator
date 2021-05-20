71dfe1390fc7634ecfbf1373f7a7192a6a5730bd
otavio25 <souzaotavio721@gmail.com>
2021-05-20 20:29:53 -0300

    add new styles and display

--- App.js
+++ App.js
@@ -1,32 +1,55 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { View, StyleSheet} from 'react-native'
 import Button from './src/components/Button'
+import Display from './src/components/Display'
 
-export default () => (
-  <View style={style.container}>
-    <View style={style.buttons}>
-      <Button label='AC'/>
-      <Button label='/'/>
-      <Button label='7'/>
-      <Button label='8'/>
-      <Button label='9'/>
-      <Button label='*'/>
-      <Button label='4'/>
-      <Button label='5'/>
-      <Button label='6'/>
-      <Button label='-'/>
-      <Button label='1'/>
-      <Button label='2'/>
-      <Button label='3'/>
-      <Button label='+'/>
-      <Button label='0'/>
-      <Button label='.'/>
-      <Button label='='/>
-    </View>
-  </View>
-)
+export default class App extends Component {
 
-const style = StyleSheet.create ({
+  state = {
+    displayValue: '0'
+  }
+
+  addDigit = n => {
+    this.setState({displayValue: n})
+  }
+
+  clearMemory = () => {
+    this.setState({displayValue: '0'})
+  }
+
+  setOperation = operation => {
+
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Display value={this.state.displayValue}/>
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory} />
+          <Button label='/' operation onClick={this.setOperation} />
+          <Button label='7' onClick={this.addDigit} />
+          <Button label='8' onClick={this.addDigit} />
+          <Button label='9' onClick={this.addDigit} />
+          <Button label='*' operation onClick={this.setOperation} />
+          <Button label='4' onClick={this.addDigit} />
+          <Button label='5' onClick={this.addDigit} />
+          <Button label='6' onClick={this.addDigit} />
+          <Button label='-' operation onClick={this.setOperation} />
+          <Button label='1' onClick={this.addDigit} />
+          <Button label='2' onClick={this.addDigit} />
+          <Button label='3' onClick={this.addDigit} />
+          <Button label='+' operation onClick={this.setOperation} />
+          <Button label='0' onClick={this.addDigit} />
+          <Button label='.' onClick={this.addDigit} />
+          <Button label='=' operation onClick={this.setOperation} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create ({
     container:{
         flex: 1
     },
