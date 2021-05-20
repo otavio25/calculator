88984a02845013e36b5346cd12ebb2606aa92bac
otavio25 <souzaotavio721@gmail.com>
2021-05-20 00:12:32 -0300

    add styles and buttons

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { View, StyleSheet} from 'react-native'
+import Button from './src/components/Button'
+
+export default () => (
+  <View style={style.container}>
+    <View style={style.buttons}>
+      <Button label='AC'/>
+      <Button label='/'/>
+      <Button label='7'/>
+      <Button label='8'/>
+      <Button label='9'/>
+      <Button label='*'/>
+      <Button label='4'/>
+      <Button label='5'/>
+      <Button label='6'/>
+      <Button label='-'/>
+      <Button label='1'/>
+      <Button label='2'/>
+      <Button label='3'/>
+      <Button label='+'/>
+      <Button label='0'/>
+      <Button label='.'/>
+      <Button label='='/>
+    </View>
+  </View>
+)
+
+const style = StyleSheet.create ({
+    container:{
+        flex: 1
+    },
+    buttons:{
+      flexDirection: 'row',
+      flexWrap: 'wrap'
+    }
+})
+
