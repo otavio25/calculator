4a27372742a0ec637b02a5f3dceb85b166dc46cc
otavio25 <souzaotavio721@gmail.com>
2021-05-21 15:45:14 -0300

    add new style

--- src/components/Display.js
+++ src/components/Display.js
@@ -11,7 +11,7 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         justifyContent: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 6)',
+        backgroundColor: '#30385D',
         alignItems: 'flex-end'
     },
     displayValue: {
